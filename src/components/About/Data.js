17f9f77055586345962d074ca70412cd72d8c9e8
Sean Zhan <seanzhan0319@hotmail.com>
2022-11-28 15:33:38 -0500

    wording change

--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -7,8 +7,8 @@ export const AboutMe = {
     'artificial intelligence; I use machine learning techniques to'+
     ' understand, generate, and manipulate 3D shapes.',
   description2:
-    'In my free time, I play ultimate frisbee, ride (and sometimes build) ' +
-    'bikes, make film photographs, and play guitar.',
+    'In my free time, I play ultimate frisbee and guitar, ' +
+    'ride (and sometimes build) bikes, and make film photographs.',
   imgStart: true,
   img: require('../../images/sean.jpg'),
   alt: 'Headshot',
